22a48c9b7ba9a048bf35ffcd0bbf94b609617942
RajanPatel123 <rajanspatel728@gmail.com>
2023-10-24 00:50:06 -0500

    Created a calender that and designed sign in page to be more themed

--- frontend/screens/Calendar/Calendar.tsx
+++ frontend/screens/Calendar/Calendar.tsx
@@ -1,40 +1,71 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import { RouteProp } from "@react-navigation/native";
+import { tabParamsList } from "./Nav"; // Ensure this is the correct path to your Nav.tsx
 import Colors from "../../constants/Colors";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomText from "../Reusables/CustomText";
-import { auth } from "../../firebase/firebaseConfig";
+import { Agenda } from "react-native-calendars";
 
+const timeToString = (time: string | number | Date) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
 
+type CalenderProps = {
+  route: RouteProp<tabParamsList, "Calender">;
+};
 
-export const Calendar = () => {
-  const userId = auth.currentUser?.uid;
-  const userEmail = auth.currentUser?.email;
+export const Calendar = ({ route }: CalendarProps) => {
+  const [items, setItems] = useState({});
+
+  const loadItems = (day: { timestamp: number; }) => {
+    setTimeout(() => {
+      setItems((currentItems) => {
+        const updatedItems = { ...currentItems };
+
+        for (let i = -15; i < 85; i++) {
+          const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+          const strTime = timeToString(time);
+
+          if (!updatedItems[strTime]) {
+            updatedItems[strTime] = [];
+
+            const numItems = Math.floor(Math.random() * 3 + 1);
+            for (let j = 0; j < numItems; j++) {
+              updatedItems[strTime].push({
+                name: 'Item for ' + strTime,
+                height: Math.max(50, Math.floor(Math.random() * 150)),
+              });
+            }
+          }
+        }
+        return updatedItems;
+      });
+    }, 1000);
+  };
+
+  // Get today's date in the required format
+  const today = new Date();
+  const currentDay = timeToString(today);
 
   return (
     <SafeAreaView style={styles.container}>
-      <CustomText style={styles.text}>Calender</CustomText>
-      {/* Just for demonstration, display the userId and userEmail */}
-      <CustomText style={styles.infoText}>User ID: {userId}</CustomText>
-      <CustomText style={styles.infoText}>User Email: {userEmail}</CustomText>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={currentDay} // This ensures the calendar opens to the current date
+        // ... other props and configurations
+      />
     </SafeAreaView>
   );
 };
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: Colors.midnightBlue,
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  infoText: {
-    fontSize: 16,
-    marginTop: 10,
+    backgroundColor: Colors.midnightBlue, // Use your color theme here
   },
 });
+

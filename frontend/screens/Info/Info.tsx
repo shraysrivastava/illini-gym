68ee498280832e4479e4834aaa7aba9015ec25f8
arnavj7 <arnavj793@gmail.com>
2024-01-16 22:22:53 -0600

    working on icon

--- frontend/screens/Info/Info.tsx
+++ frontend/screens/Info/Info.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
 import CustomText from '../Reusables/CustomText';
 
 const InfoScreen = () => {
     return (
         <View style={styles.container}>
-            <CustomText style={styles.text}>Note: all data is sourced from https://apps2.campusrec.illinois.edu/checkins/live and is updated periodically. </CustomText>
+            <CustomText style={styles.heading}>Data Source Information</CustomText>
+            <CustomText style={styles.text}>
+                Note: All data is sourced from
+            </CustomText>
+            <CustomText style={styles.sourceUrl}>
+                https://apps2.campusrec.illinois.edu/checkins/live
+            </CustomText>
+            <CustomText style={styles.text}>
+                and is updated periodically.
+            </CustomText>
         </View>
     );
 };
@@ -17,10 +26,27 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: Colors.midnightBlue,
+        padding: 20,
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        color: Colors.uiucOrange,
     },
     text: {
-        fontSize: 20,
-        fontWeight: 'bold',
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    sourceUrl: {
+        fontSize: 18,
+        color: Colors.uiucOrange,
+        marginBottom: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
     },
 });
 

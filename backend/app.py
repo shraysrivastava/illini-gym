8273e215cb61cf55334d70b4ce19d62ea6f29da1
Shray Srivastava <shray3420@gmail.com>
2024-01-17 22:04:02 -0600

    made some scraper changes

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,10 @@
-# app.py
-
 from flask import Flask, jsonify
 from scraper import scrape_and_update
-from scraper import scrape_and_update_cerce
 import firebase_admin
 from firebase_admin import credentials, firestore
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -15,13 +12,28 @@ app = Flask(__name__)
 def update_firestore():
     try:
         scrape_and_update()
-        scrape_and_update_cerce()
         return jsonify({"status": "Firestore updated with scraped data."}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 def run_flask():
     app.run(debug=True, use_reloader=False)  # Disable the reloader
+
+def is_gym_open(now):
+    # Define gym hours
+    gym_hours = {
+        "Monday": (6, 23),
+        "Tuesday": (6, 23),
+        "Wednesday": (6, 23),
+        "Thursday": (6, 23),
+        "Friday": (6, 22),
+        "Saturday": (9, 22),
+        "Sunday": (9, 22)
+    }
+
+    day_name = now.strftime("%A")
+    open_hour, close_hour = gym_hours.get(day_name, (0, 0))
+    return open_hour <= now.hour < close_hour
 
 if __name__ == '__main__':
     # Start the Flask server in a separate thread
@@ -30,11 +42,15 @@ if __name__ == '__main__':
 
     # Add the periodic update loop
     while True:
-        try:
-            scrape_and_update()  # Call the function to scrape and update Firebase
-            # scrape_and_update_cerce()
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"Firestore updated at {current_time}. Waiting for the next update...")
-            time.sleep(1200)  # Sleep for 1 hour (adjust as needed)
-        except Exception as e:
-            print(f"Error updating Firestore: {str(e)}")
+        current_time = datetime.now()
+        if is_gym_open(current_time):
+            try:
+                scrape_and_update()  # Call the function to scrape and update Firebase
+                print(f"[{current_time.strftime('%Y-%m-%d %H:%M:%S')}] Firestore successfully updated.")
+            except Exception as e:
+                print(f"[{current_time.strftime('%Y-%m-%d %H:%M:%S')}] Error updating Firestore: {e}")
+        else:
+            next_check_time = current_time + timedelta(minutes=20)
+            print(f"[{current_time.strftime('%Y-%m-%d %H:%M:%S')}] Gym is closed. Next check at {next_check_time.strftime('%Y-%m-%d %H:%M:%S')}.")
+        
+        time.sleep(1200)  # Sleep for 20 minutes

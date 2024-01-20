36948f2f77e26b0904065e133885082ab4c05b45
arnavj7 <arnavj793@gmail.com>
2024-01-20 13:29:41 -0600

    bug fix

--- frontend/screens/Maps/MapsHome.tsx
+++ frontend/screens/Maps/MapsHome.tsx
@@ -356,7 +356,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     width: "100%",
-    height: "60%",
+    height: "50%",
     backgroundColor: Colors.midnightBlue,
     padding: "5%",
     alignItems: "center",
